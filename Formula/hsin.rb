f008d6a420626a59720e936a72466d49bba9d4f9
Kitsune <KyuubiRan@outlook.com>
2026-07-26 12:21:42 +0800

    hsin 0.1.0

--- Formula/hsin.rb
+++ Formula/hsin.rb
@@ -9,18 +9,18 @@ class Hsin < Formula
   on_macos do
     on_arm do
       url "https://github.com/KyuubiRan/hsin.rs/releases/download/v#{version}/hsin-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_AARCH64_APPLE_DARWIN_SHA256"
+      sha256 "dfce16c933e23c89d52d1b84a24c4f3caf1252d95e3d4d44d3f0c079a1d052f1"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/KyuubiRan/hsin.rs/releases/download/v#{version}/hsin-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_AARCH64_UNKNOWN_LINUX_GNU_SHA256"
+      sha256 "3c2d337f5ccbbd3bae95a4a57cdec9f73d66120ff092b183b609d2934e9fd12d"
     end
     on_intel do
       url "https://github.com/KyuubiRan/hsin.rs/releases/download/v#{version}/hsin-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_X86_64_UNKNOWN_LINUX_GNU_SHA256"
+      sha256 "cbdcbcc165d42b11e4e78189acad1629cd23053caa12f852eb67ff99ef85766c"
     end
   end
 
