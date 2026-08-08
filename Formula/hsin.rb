660be52f849711f1f7a4f8fae013f1822554bfb0
Kitsune <KyuubiRan@outlook.com>
2026-08-08 22:38:53 +0800

    hsin: update to 0.1.8

--- Formula/hsin.rb
+++ Formula/hsin.rb
@@ -1,7 +1,7 @@
 class Hsin < Formula
   desc "Daemon-first provider switcher for Codex and Claude Code"
   homepage "https://github.com/KyuubiRan/hsin.rs"
-  version "0.1.7"
+  version "0.1.8"
   license "MIT"
 
   depends_on arch: :arm64 if OS.mac?
@@ -9,18 +9,18 @@ class Hsin < Formula
   on_macos do
     on_arm do
       url "https://github.com/KyuubiRan/hsin.rs/releases/download/v#{version}/hsin-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "1fa10846ae7542167f9cc3af9ab356ff3a49dfb5e5fce42f6ff74944ba0a5bdb"
+      sha256 "8218cd93563ce760ded8700cfcb73ffdee61752d9124f45d38c017faa431478b"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/KyuubiRan/hsin.rs/releases/download/v#{version}/hsin-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "6a95ac89ee0df07506e0617e11a1a3c51da8cb1a412170bffee820b799488564"
+      sha256 "c9b27cedf96161a26767d3d33096877c289cf6f52c1b052aec2771ee0aa0c2c5"
     end
     on_intel do
       url "https://github.com/KyuubiRan/hsin.rs/releases/download/v#{version}/hsin-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e1920fa67a84f916f1c0215b88dad13b4fec5fff8259ba526b0a49b0f2948b98"
+      sha256 "b56edbfde36cf016f9542db26b94b793c830c79ab26b4ab231571b8084362e74"
     end
   end
 
